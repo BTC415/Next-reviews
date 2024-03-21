9f1e621945d0a561bfe0ec1ed7287912dac006c0
dodger213 <alenschmitt415@gmail.com>
2024-03-21 15:19:58 +1100

    modified heading component

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading({ children }) {
   return (
-    <Heading className="font-bold pb-3 text-2xl">
+    <h1 className="font-bold pb-3 text-2xl">
       {children}
-    </Heading>
+    </h1>
   )
 }
