f8472895d6c0f735aadfb032215cec774c828452
dodger213 <alenschmitt415@gmail.com>
2024-03-21 17:33:33 +1100

    Read data from markdown file

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Homepage() {
       </p>
       <div className="bg-white border rounded shadow hover:shadow-xl w-80 sm:w-full">
         <Link href="/reviews/stardew-valley" className="flex flex-col sm:flex-row">
-          <img src="/images/stardew-valley.jpg" alt="" width="640" height="360" 
+          <img src="/images/stardew-valley.jpg" alt="" width="640" height="360"
           className="rounded-t sm:rounded-l rounded-r-none" />
           <h2 className="py-1 font-semibold text-center font-orbitron">
             Stardew Valley
--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,12 +1,20 @@
+import { readFile } from 'node:fs/promises'
+import { marked } from 'marked'
+import matter from 'gray-matter'
 import Heading from "@/components/Heading"
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
+  const { content, data:{title, date, image} } = matter(text)
+  const html = marked(content)
+
   return (
     <>
-      <Heading>Reviews</Heading>
-      <img src="/images/stardew-valley.jpg" alt="" width="640" height="360" className="mb-2 rounded"/>
+      <Heading>{title}</Heading>
+      <p className='italic pb-2'>{date}</p>
+      <img src={image} alt="" width="640" height="360" className="mb-2 rounded" />
       <p>
-        Here we'll list all the reviews.
+        <article dangerouslySetInnerHTML={{ __html: html }} className='prose prose-slate' />
       </p>
     </>
   )
