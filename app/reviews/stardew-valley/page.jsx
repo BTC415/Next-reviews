88b46d4bd97ecbad1ed642ac49a4d488a3ae3e2a
Mit213 <mitsurukudo213@gmail.com>
2024-03-21 14:39:07 +1000

    Added images to each review page

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,7 @@ export default function ReviewsPage() {
   return (
     <>
       <Heading>Reviews</Heading>
+      <img src="/images/stardew-valley.jpg" alt="" width="640" height="360" className="mb-2 rounded"/>
       <p>
         Here we'll list all the reviews.
       </p>
